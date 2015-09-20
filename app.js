849fd0d977572275d798cc4c424a94c747520509
hacevedo <haley.acevedo2@gmail.com>
2015-09-19 20:59:45 -0500

    http post

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.controller("crawlCtrl",  ['$scope', '$http', 'apiService', function($scope, 
 	$scope.sub_cat = "bars";
 	$scope.num = "5";
 
+	$scope.start = {
+		"latitude" : "41",
+		"longitude" : "42"
+	};
+
 	$scope.getResults = function (loc, cat, sub_cat){
 		var url = "http://api.tripadvisor.com/api/partner/2.0/location/" + loc + "/" + cat + "?key=4FDDE1CDF4C2402980BABF84989B111D";
 		if(sub_cat !== "") {
@@ -51,11 +56,37 @@ app.controller("crawlCtrl",  ['$scope', '$http', 'apiService', function($scope, 
 			console.log("successful retrieval!");
 			console.log(response);
 			$scope.list= response.data.data;
+			$scope.postPlaceList();
 		});
 
 	};
 
 	$scope.removePlace = function(index){
 		$scope.list.splice(index,1);
+		$scope.postPlaceList();
+	};
+
+	$scope.postPlaceList = function(){
+		var url = "localhost:8000";
+		console.log("in post");
+		var placeData = {
+			"start" : {
+				"lat" : $scope.start.latitude,
+				"long" : $scope.start.longitude
+			},
+			"destinations" : []
+		};
+		for(var i = 0; i < $scope.num; i++){
+			var placeObj = {
+				"lat" : $scope.list[i].latitude,
+				"long" : $scope.list[i].longitude,
+				"id" : $scope.list[i].location_id
+			};
+			placeData.destinations.push(placeObj);
+		}
+		console.log(placeData);
+		$http.post(url, placeData).then(function(){
+			console.log("successful post!");
+		});
 	};
 }]);
